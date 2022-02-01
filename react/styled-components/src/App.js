798dad29e0d0ac721c04fe7d4d8aca47f1874805
Raajzz <raajkumar.s2020@vitstudent.ac.in>
2022-02-02 00:01:00 +0530

    Started learning styled components

---
+++ react/styled-components/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Component from "./components/styles/Component.styles";
+
+const App = () => {
+  return (
+    <Component>
+      <h1>Hello World</h1>
+    </Component>
+  );
+};
+
+export default App;
---
+++ react/styled-components/src/components/styles/Component.styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const Component = styled.div`
+  text-align: center;
+`;
+
+export default Component;
