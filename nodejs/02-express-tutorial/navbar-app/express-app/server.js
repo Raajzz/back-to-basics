88972a756dd268c212bdac765d9a29843c178b9b
Raajzz <raajkumar.s2020@vitstudent.ac.in>
2022-03-01 13:30:27 +0530

    Completed static assets, need to start API Vs. SSR

--- nodejs/02-express-tutorial/navbar-app/express-app/server.js
+++ nodejs/02-express-tutorial/navbar-app/express-app/server.js
@@ -2,22 +2,45 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../index.html"));
-});
+// the manual way without app.use(express.static()) method
 
-app.get("/styles.css", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../styles.css"));
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../index.html"));
+// });
 
-app.get("/logo.svg", (req, res) => {
-  res.sendFile(path);
-});
+// app.get("/styles.css", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../styles.css"));
+// });
 
-app.all("*", (req, res) => {
+// app.get("/logo.svg", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../logo.svg"));
+// });
+
+// app.get("/browser-app.js", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../browser-app.js"));
+// });
+
+// app.all("*", (req, res) => {
+//   res.send("<h1>404 Page Does Not Exist!</h1>");
+// });
+
+// app.listen(3001, () => {
+//   console.log("Listening on port 3001");
+// });
+
+// a better way
+
+// setup static middleware
+
+app.use(express.static(path.resolve(__dirname, "./public")))
+
+app.all("*", (req, res)=>{
   res.send("<h1>404 Page Does Not Exist!</h1>");
-});
+})
 
 app.listen(3001, () => {
   console.log("Listening on port 3001");
 });
+
+// better way ++ 
+
